e2d6467aeabb2641151934cbbeb94bf6f0ce509e
kamrank89 <68290007+kamrank89@users.noreply.github.com>
2022-07-06 11:43:59 -0500

    module 3 work almost done

--- –force/app_server/controllers/travel.js
+++ –force/app_server/controllers/travel.js
@@ -1,6 +1,11 @@
+/* Requiring fs */
+const fs = require("fs");
+
+const trips = JSON.parse(fs.readFileSync("app_server/data/trips.json", "utf8"));
+
 /* Get Travel View */
 const travel = (req, res) => {
-  res.render("travel", { title: "Travlr Getaways" });
+  res.render("travel", { title: "Travlr Getaways", trips });
 };
 module.exports = {
   travel,
