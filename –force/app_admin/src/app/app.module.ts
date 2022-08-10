ef094617b9172c71546eae2521f5e729e98c291f
kamrank89 <68290007+kamrank89@users.noreply.github.com>
2022-08-10 09:38:59 -0500

    upto 18:49 of the video is done

--- –force/app_admin/src/app/app.module.ts
+++ –force/app_admin/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { ReactiveFormsModule } from "@angular/forms";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { TripListingComponent } from "./trip-listing/trip-listing.component";
@@ -9,8 +10,9 @@ import { TripCardComponent } from "./trip-card/trip-card.component";
 import { TripDataService } from "./services/trip-data.service";
 import { AddTripComponent } from "./add-trip/add-trip.component";
 import { AppRoutingModule } from "./app-router.module";
-import { EditTripComponent } from './edit-trip/edit-trip.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { EditTripComponent } from "./edit-trip/edit-trip.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { LoginComponent } from "./login/login.component";
 
 @NgModule({
   declarations: [
@@ -20,12 +22,14 @@ import { NavbarComponent } from './navbar/navbar.component';
     AddTripComponent,
     EditTripComponent,
     NavbarComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     ReactiveFormsModule,
     AppRoutingModule,
+    FormsModule,
   ],
   providers: [TripDataService],
   bootstrap: [AppComponent],
